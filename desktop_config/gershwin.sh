537862c0479cbe6c6f06adce4e4342cbebc657f0
probonopd <probonopd@users.noreply.github.com>
2026-01-17 19:23:09 +0100

    Increase verbosity, use permalink
    Updated gershwin.sh to enable debug mode and modify URLs for system artifacts.

--- desktop_config/gershwin.sh
+++ desktop_config/gershwin.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-set -e -u
+set -e -u -x
 
 . "${cwd}/common_config/autologin.sh"
 . "${cwd}/common_config/base-setting.sh"
 . "${cwd}/common_config/finalize.sh"
 . "${cwd}/common_config/setuser.sh"
-
-gershwin_setup()
-{
-  chmod +x "${cwd}/overlays/uzip/gershwin/files/usr/local/bin/gershwin-x11"
-  cp -R "${cwd}/overlays/uzip/gershwin/files/" "${release}"
-}
 
 setup_xinit()
 {
@@ -25,7 +19,7 @@ install_system()
   # Make binaries from FreeBSD 14 usable on FreeBSD 15
   [ -e "${release}"/lib/libutil.so.9 ] || [ ! -e "${release}"/lib/libutil.so.10 ] || ln -s "${release}"libutil.so.10 "${release}"/lib/libutil.so.9
   # Install /System (built in gershwin-build repository)
-  u="https://api.cirrus-ci.com/v1/artifact/task/5361614007828480/system/artifacts/FreeBSD/14/amd64/system.txz"
+  u="https://api.cirrus-ci.com/v1/artifact/github/gershwin-desktop/gershwin-build/data/system/artifacts/FreeBSD/14/amd64/system.txz"
   curl -sSf "$u" -o "$(basename "$u")"
   tar -xJf system.txz -C "${release}"/
   # Install and enable loginwindow service
@@ -38,7 +32,6 @@ install_system()
 patch_etc_files
 community_setup_liveuser_gershwin
 community_setup_autologin_gershwin
-gershwin_setup
 setup_xinit
 install_system
 final_setup
